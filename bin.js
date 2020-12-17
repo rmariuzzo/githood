0b562bb2bd7210244517d6dc659c60952f8da995
Rubens Mariuzzo <rubens@mariuzzo.com>
2020-12-16 23:32:31 -0400

    Simplify bin.js

--- bin.js
+++ bin.js
@@ -1,15 +1,3 @@
 #!/usr/bin/env node
 /* eslint-disable */
-try {
-  // If ran with `npx` against the GitHub repo and dist files are not available
-  // then it will fail.
-  require('./dist')
-} catch {
-  console.error(
-    "githood: can't be run right now, please try again in a few seconds..."
-  )
-  console.error(
-    'githood: if the problem persist check: https://github.com/rmariuzzo/githood/actions'
-  )
-  process.exit(9)
-}
+require('./dist')
