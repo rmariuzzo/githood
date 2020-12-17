5e438addcc5b10bf85a1912881b0e040186a67cc
Rubens Mariuzzo <rubens@mariuzzo.com>
2020-12-16 23:16:05 -0400

    Tune prettier.

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,12 @@ import { errors } from './errors'
 
 const { help, debug, org, list, count } = parseArgv(process.argv.slice(2))
 
+/* prettier-ignore */
 if (help) {
   console.info(`githood`)
   console.info(`Usage: githood [git-sub-command]`)
   console.info(`Options:`)
-  console.info(
-    `  --org        Run commands in repos belonging to a GitHub org.`
-  )
+  console.info(`  --org        Run commands in repos belonging to a GitHub org.`)
   console.info(`  --list       List all git repos.`)
   console.info(`  --count      Count all git repos.`)
   console.info(`  --help       Show this help message.`)
